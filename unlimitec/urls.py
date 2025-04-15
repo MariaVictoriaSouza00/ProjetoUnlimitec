132b4aa1848857753d7be6fd814e91921efdfc25
MARIA VICTORIA SOUZA <105884630+MariaVictoriaSouza00@users.noreply.github.com>
2025-04-14 22:59:21 -0400

    adequacao para deploy

--- unlimitec/urls.py
+++ unlimitec/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from appUsuario.views import login_usuario
+from appPesquisa.views import tela_index
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', login_usuario, name='login_usuario'), 
+    path('', tela_index, name='tela_index'), 
     path('usuario/', include('appUsuario.urls')),  
     path('pesquisa/', include('appPesquisa.urls')), 
 ]
