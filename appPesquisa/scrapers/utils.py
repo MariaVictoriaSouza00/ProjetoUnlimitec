a77564f7d862bfe0b843ead6a453c37ca4ad8360
MARIA VICTORIA SOUZA <105884630+MariaVictoriaSouza00@users.noreply.github.com>
2025-05-13 12:10:54 -0400

    adicionar dados do scrpa para producao

--- appPesquisa/scrapers/utils.py
+++ appPesquisa/scrapers/utils.py
@@ -1,30 +1,16 @@
-import os
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 def configurar_driver():
     options = Options()
-    options.add_argument("--headless")  # Executa sem interface gráfica
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    
-    # Caminhos para o Chrome e o Chromium
-    chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe"  # Caminho do Chrome
-    chromium_path = "C:/chromium/chrome.exe"  # Caminho do Chromium (caso você tenha instalado)
+    options.headless = True  # Para rodar sem interface gráfica
 
-    # Verifica se o Google Chrome está instalado, caso contrário, usa o Chromium
-    if not os.path.exists(chrome_path):
-        if os.path.exists(chromium_path):
-            print("Chrome não encontrado, usando Chromium.")
-            options.binary_location = chromium_path  # Usar o Chromium
-        else:
-            print("Chrome nem Chromium encontrado! Certifique-se de que um dos navegadores esteja instalado.")
-            raise Exception("Navegador não encontrado")
-    else:
-        options.binary_location = chrome_path  # Usar o Google Chrome
+    # Se você sabe o caminho do seu navegador, pode adicionar ele aqui:
+    options.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"  # Exemplo no Windows
 
-    # Configura o WebDriver
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    # Usa o webdriver_manager para configurar o ChromeDriver automaticamente
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
     return driver
