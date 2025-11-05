9043cd05f9a20abed4ad6f163e22460b4cd77698
MARIA VICTORIA SOUZA <105884630+MariaVictoriaSouza00@users.noreply.github.com>
2025-11-04 23:08:49 -0400

    divisao e organizacao de pastas

--- appPesquisa/urls.py
+++ appPesquisa/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('index/', tela_index, name='tela_index'),
     path('healthz', views.health_check, name='health_check'),
     path('buscar-titulos/', buscar_titulos_ajax, name='buscar_titulos_ajax'),
-     path('fomento/', views.fomento, name='fomento'),
+    path('fomento/', views.fomento, name='fomento'),
     path('definicao/', views.pesquisar_definicao, name='definicao'),
+    path('conhecaPlataforma/', views.conhecaPlataforma, name='conhecaPlataforma'),
+    path('contatenos/', views.contatenos, name='contatenos')
 ]
--- appPesquisa/views.py
+++ appPesquisa/views.py
@@ -45,11 +45,21 @@ def obter_todos_titulos():
 def tela_index(request):
     return render(request, 'pesquisa/index.html')
 
+def tela_conatenos(request):
+    return render(request, 'pesquisa/contatenos.html')
+
 def fomento(request):
     return render(request, 'pesquisa/fomento.html')
 
 def definicao(request):
     return render(request, 'pesquisa/definicao.html')
+
+
+def conhecaPlataforma(request):
+    return render(request, 'base/navbar/conhecaPlataforma.html')
+
+def contatenos(request):
+    return render(request, 'base/navbar/contatenos.html')
 
 # ========================== Sinonímia ==========================
 def obter_sinonimos_api(termo):
